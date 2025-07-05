0fbe8099080bd9724c1135ef002598e1eed8df24
Taufan Augusta <taufan.augusta@protonmail.com>
2025-07-05 11:27:28 +0700

    feat: locate and center

--- src/OpenStreetMap.Blazor/wwwroot/js/leafletInterop.js
+++ src/OpenStreetMap.Blazor/wwwroot/js/leafletInterop.js
@@ -9,6 +9,34 @@ window.leafletInterop = {
     window._leafletMap = map;
   },
 
+  locateAndCenter: function () {
+    // prevents errors if the map isn't initialized yet.
+    if (!window._leafletMap) return;
+
+    // html5 geolocation api
+    navigator.geolocation.getCurrentPosition(
+
+      // parameters
+      // success callback
+      function (position) {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+
+        window._leafletMap.setView([lat, lng], 13);
+
+        L.marker([lat, lng])
+          .addTo(window._leafletMap)
+          .bindPopup("You are here")
+          .openPopup();
+      },
+
+      // error callback
+      function (err) {
+        console.warn("Geolocation error:", err.message);
+      }
+    );
+  },
+
   addMarker: function (lat, lng, popupText) {
     if (!window._leafletMap) return;
 
